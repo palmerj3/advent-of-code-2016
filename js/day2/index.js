6d3316591b931cb811aeef37e6e03d66206ad127
Jason Palmer <jason@jason-palmer.com>
2016-12-02 09:11:11 -0500

    JS solution to day 2

---
+++ js/day2/index.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const inputRaw = fs.readFileSync(path.join(process.cwd(), 'input'), 'utf8');
+const inputRows = inputRaw.split("\n");
+
+const keypad = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+];
+
+// row,column
+const pos = [1,2];
+
+let code = [];
+
+inputRows.forEach((r) => {
+  let inputTokens = r.split('');
+
+  if (inputTokens.length > 0) {
+    inputTokens.forEach((t) => {
+      switch(t) {
+        case 'U':
+          keypad[pos[0]-1] ? pos[0]-- : pos[0];
+          break;
+        case 'D':
+          keypad[pos[0]+1] ? pos[0]++ : pos[0];
+          break;
+        case 'L':
+          keypad[pos[0]][pos[1]-1] ? pos[1]-- : pos[1];
+          break;
+        case 'R':
+          keypad[pos[0]][pos[1]+1] ? pos[1]++ : pos[1];
+          break;
+      }
+    });
+
+    code.push(keypad[pos[0]][pos[1]]);
+  }
+});
+
+console.log(code.join(''));
